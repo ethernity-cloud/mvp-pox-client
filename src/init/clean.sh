e543a37289752b7f38a141fd95560db479ccdf6e
Iosif Peterfi <iosif.peterfi@gmail.com>
2020-12-19 11:17:44 +0100

    * Updated bash scripts for windows git bash compatibility

--- src/init/clean.sh
+++ src/init/clean.sh
@@ -2,13 +2,22 @@
 cd .tmp/go-ipfs/
 ./ipfs daemon 2>&1 >> /dev/null &
 
-IP=`getent hosts ipfs.ethernity.cloud | awk '{print $1}'`
+IP=`nslookup ipfs.ethernity.cloud 2>&1 | grep -A 1 ethernity.cloud | grep 'Address' | awk '{print $2}'`
 
-until ./ipfs swarm connect /ip4/$IP/tcp/4001/ipfs/QmRBc1eBt4hpJQUqHqn6eA8ixQPD3LFcUDsn6coKBQtia5
-do
-  sleep 1
-done
-./ipfs bootstrap add /ip4/$IP/tcp/4001/ipfs/QmRBc1eBt4hpJQUqHqn6eA8ixQPD3LFcUDsn6coKBQtia5
+if [ "$OS" == "Windows_NT" ]
+then
+    until cmd //c "ipfs swarm connect /ip4/${IP}/tcp/4001/ipfs/QmRBc1eBt4hpJQUqHqn6eA8ixQPD3LFcUDsn6coKBQtia5"
+    do
+        sleep 1
+    done
+    cmd //c "ipfs bootstrap add /ip4/${IP}/tcp/4001/ipfs/QmRBc1eBt4hpJQUqHqn6eA8ixQPD3LFcUDsn6coKBQtia5"
+else
+    until ./ipfs swarm connect /ip4/${IP}/tcp/4001/ipfs/QmRBc1eBt4hpJQUqHqn6eA8ixQPD3LFcUDsn6coKBQtia5
+    do
+        sleep 1
+    done
+    ./ipfs bootstrap add /ip4/${IP}/tcp/4001/ipfs/QmRBc1eBt4hpJQUqHqn6eA8ixQPD3LFcUDsn6coKBQtia5
+fi
 
 for hash in `./ipfs pin ls | awk '{print $1}'`; do ./ipfs pin rm $hash 2>&1 >> /dev/null ; done
 
--- src/init/init.sh
+++ src/init/init.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-if [ -x "$(command -v python3)" ]; then
-   PYTHON=python3
+if [ "$OS" == "Windows_NT" ]
+then
+    PYTHON_BIN=python
+else
+    PYTHON_BIN=python3
+fi
+
+
+if [ -x "$(command -v ${PYTHON_BIN})" ]; then
+   PYTHON=${PYTHON_BIN}
 else
    exit 1
 fi
@@ -16,20 +24,44 @@ $PIP install ipfshttpclient web3 --upgrade
 
 mkdir .tmp
 cd .tmp
-wget https://github.com/ipfs/go-ipfs/releases/download/v0.6.0/go-ipfs_v0.6.0_linux-386.tar.gz 2>&1 >> /dev/null
-tar zxvf go-ipfs_v0.6.0_linux-386.tar.gz 2>&1 >> /dev/null
+
+IPFS_VERSION='v0.6.0'
+MIRROR="https://dist.ipfs.io/go-ipfs/${IPFS_VERSION}/"
+
+if [ "$OS" == "Windows_NT" ]
+then
+    FILENAME="go-ipfs_${IPFS_VERSION}_windows-amd64.zip"
+    EXTRACT_COMMAND='unzip'
+else
+    FILENAME="go-ipfs_${IPFS_VERSION}_linux-amd64.tar.gz"
+    EXTRACT_COMMAND='tar -zxvf'
+fi
+
+URL=${MIRROR}${FILENAME}
+
+curl ${URL} --output ${FILENAME} 2>&1
+${EXTRACT_COMMAND} ${FILENAME} 2>&1
 cd go-ipfs
-./ipfs init 2>&1 >> /dev/null
-./ipfs daemon 2>&1 >> /dev/null &
+./ipfs init
+./ipfs daemon &
 
-IP=`getent hosts ipfs.ethernity.cloud | awk '{print $1}'`
+IP=`nslookup ipfs.ethernity.cloud 2>&1 | grep -A 1 ethernity.cloud | grep 'Address' | awk '{print $2}'` 2>&1 > /dev/null
 
-until ./ipfs swarm connect /ip4/$IP/tcp/4001/ipfs/QmRBc1eBt4hpJQUqHqn6eA8ixQPD3LFcUDsn6coKBQtia5
-do
-  sleep 1
-done
+if [ "$OS" == "Windows_NT" ]
+then
+    until cmd //c "ipfs swarm connect /ip4/${IP}/tcp/4001/ipfs/QmRBc1eBt4hpJQUqHqn6eA8ixQPD3LFcUDsn6coKBQtia5"
+    do
+	sleep 1
+    done
 
-./ipfs bootstrap add /ip4/$IP/tcp/4001/ipfs/QmRBc1eBt4hpJQUqHqn6eA8ixQPD3LFcUDsn6coKBQtia5
+    cmd //c "ipfs bootstrap add /ip4/${IP}/tcp/4001/ipfs/QmRBc1eBt4hpJQUqHqn6eA8ixQPD3LFcUDsn6coKBQtia5"
+else
+    until ./ipfs swarm connect /ip4/${IP}/tcp/4001/ipfs/QmRBc1eBt4hpJQUqHqn6eA8ixQPD3LFcUDsn6coKBQtia5
+    do
+	sleep 1
+    done
+    ./ipfs bootstrap add /ip4/${IP}/tcp/4001/ipfs/QmRBc1eBt4hpJQUqHqn6eA8ixQPD3LFcUDsn6coKBQtia5
+fi
 
 cd ../..
 mkdir certs
