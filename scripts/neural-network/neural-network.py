f58e3b6098d268eb6e0c5c732ab883ca30ecd05e
Iosif Peterfi <iosif.peterfi@gmail.com>
2021-03-23 12:00:15 +0100

    * Added neural-network example that uses numpy, scipy, scikit-learn

---
+++ scripts/neural-network/neural-network.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+
+# Authors: Yann N. Dauphin, Vlad Niculae, Gabriel Synnaeve
+# License: BSD
+
+import numpy as np
+
+from scipy.ndimage import convolve
+from sklearn import linear_model, datasets, metrics
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import BernoulliRBM
+from sklearn.pipeline import Pipeline
+from sklearn.base import clone
+
+# import warnings filter
+from warnings import simplefilter
+# ignore all future warnings
+simplefilter(action='ignore', category=FutureWarning)
+
+import configparser
+
+config = configparser.ConfigParser()
+config.read('fileset/config')
+
+# #############################################################################
+# Setting up
+
+def nudge_dataset(X, Y):
+    """
+    This produces a dataset 5 times bigger than the original one,
+    by moving the 8x8 images in X around by 1px to left, right, down, up
+    """
+    direction_vectors =  [
+        [[0, 1, 0],
+         [0, 0, 0],
+         [0, 0, 0]],
+
+        [[0, 0, 0],
+         [1, 0, 0],
+         [0, 0, 0]],
+
+        [[0, 0, 0],
+         [0, 0, 1],
+         [0, 0, 0]],
+
+        [[0, 0, 0],
+         [0, 0, 0],
+         [0, 1, 0]]]
+
+
+    def shift(x, w):
+        return convolve(x.reshape((8, 8)), mode='constant', weights=w).ravel()
+
+    X = np.concatenate([X] +
+                       [np.apply_along_axis(shift, 1, X, vector)
+                        for vector in direction_vectors])
+    Y = np.concatenate([Y for _ in range(5)], axis=0)
+    return X, Y
+
+
+# Load Data
+X, y = datasets.load_digits(return_X_y=True)
+X = np.asarray(X, 'float32')
+X, Y = nudge_dataset(X, y)
+X = (X - np.min(X, 0)) / (np.max(X, 0) + 0.0001)  # 0-1 scaling
+
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, test_size=0.2, random_state=0)
+
+# Models we will use
+logistic = linear_model.LogisticRegression(solver='newton-cg', tol=1)
+rbm = BernoulliRBM(random_state=0, verbose=True)
+
+rbm_features_classifier = Pipeline(
+    steps=[('rbm', rbm), ('logistic', logistic)])
+
+# #############################################################################
+# Training
+
+# Hyper-parameters. These were set by cross-validation,
+# using a GridSearchCV. Here we are not performing cross-validation to
+# save time.
+rbm.learning_rate = 0.06
+rbm.n_iter = int(config['neural_network']['iterations'])
+# More components tend to give better prediction performance, but larger
+# fitting time
+rbm.n_components = 100
+logistic.C = 6000
+
+# Training RBM-Logistic Pipeline
+rbm_features_classifier.fit(X_train, Y_train)
+
+# Training the Logistic regression classifier directly on the pixel
+raw_pixel_classifier = clone(logistic)
+raw_pixel_classifier.C = 100.
+raw_pixel_classifier.fit(X_train, Y_train)
+
+# #############################################################################
+# Evaluation
+
+Y_pred = rbm_features_classifier.predict(X_test)
+print("Logistic regression using RBM features:\n%s\n" % (
+    metrics.classification_report(Y_test, Y_pred)))
+
+Y_pred = raw_pixel_classifier.predict(X_test)
+print("Logistic regression using raw pixel features:\n%s\n" % (
+    metrics.classification_report(Y_test, Y_pred)))
