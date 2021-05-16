c1941e93c132d4cfe8616a9433ee68960747bbcb
Iosif Peterfi <iosif.peterfi@gmail.com>
2021-05-16 10:24:54 +0200

    * Improved initial setup on Windows by adding VS Build Tools * Fixed ipfs connection to Ethernity IPFS network during first init * Minor bug fixes and verbosity adjustments for init scripts

--- src/init/init_ipfs_python.sh
+++ src/init/init_ipfs_python.sh
@@ -3,6 +3,11 @@
 if [ "$OS" == "Windows_NT" ]
 then
     PYTHON_BIN=python
+    echo ""
+    echo "Installing Visual Studio C++ Build Tools"
+    src/init/deps/Win/vs_BuildTools.exe --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 Microsoft.VisualStudio.Component.Windows10SDK.18362 --passive --norestart --addProductLang en-us --installWhileDownloading
+    echo -en "Press ENTER when Setup is finished. "
+    read -r RESPONSE
 else
     PYTHON_BIN=python3
 fi
@@ -20,9 +25,12 @@ else
    exit 2
 fi
 
-$PIP install ipfshttpclient web3 psutil --upgrade
+echo -en "Installing Python3 dependencies... "
+$PIP install --upgrade pip  2>&1 > /dev/null
+$PIP install ipfshttpclient web3 psutil --upgrade 2>&1 > /dev/null
+echo "done"
 
-mkdir .tmp
+[ -d .tmp ] || mkdir .tmp
 cd .tmp
 
 IPFS_VERSION='v0.6.0'
@@ -31,7 +39,7 @@ MIRROR="https://dist.ipfs.io/go-ipfs/${IPFS_VERSION}/"
 if [ "$OS" == "Windows_NT" ]
 then
     FILENAME="go-ipfs_${IPFS_VERSION}_windows-amd64.zip"
-    EXTRACT_COMMAND='unzip'
+    EXTRACT_COMMAND='unzip -o'
 else
     FILENAME="go-ipfs_${IPFS_VERSION}_linux-amd64.tar.gz"
     EXTRACT_COMMAND='tar -zxvf'
@@ -39,13 +47,16 @@ fi
 
 URL=${MIRROR}${FILENAME}
 
-curl ${URL} --output ${FILENAME} 2>&1
-${EXTRACT_COMMAND} ${FILENAME} 2>&1
+echo "Installing and running go-ipfs"
+curl ${URL} --output ${FILENAME}
+${EXTRACT_COMMAND} ${FILENAME} 2>&1 > /dev/null
 cd go-ipfs
-./ipfs init
-./ipfs daemon &
+./ipfs init 2>&1 > /dev/null
+./ipfs daemon 2>&1 > /dev/null &
 
-IP=`nslookup ipfs.ethernity.cloud 2>&1 | grep -A 1 ethernity.cloud | grep 'Address' | awk '{print $2}'` 2>&1 > /dev/null
+echo "Conneting to Ethernity IPFS network"
+
+IP=`nslookup ipfs.ethernity.cloud 2>&1 | grep -A 1 ethernity.cloud | grep 'Address' | awk '{print $2}'`
 
 if [ "$OS" == "Windows_NT" ]
 then
@@ -64,6 +75,6 @@ else
 fi
 
 cd ../..
-mkdir certs
+[ -d certs ] || mkdir certs
 
 touch .init-done
--- src/init/init_python.sh
+++ src/init/init_python.sh
@@ -3,6 +3,11 @@
 if [ "$OS" == "Windows_NT" ]
 then
     PYTHON_BIN=python
+    echo ""
+    echo "Installing Visual Studio C++ Build Tools"
+    src/init/deps/Win/vs_BuildTools.exe --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 Microsoft.VisualStudio.Component.Windows10SDK.18362 --passive --norestart --addProductLang en-us --installWhileDownloading
+    echo -en "Press ENTER when Setup is finished. "
+    read -r RESPONSE
 else
     PYTHON_BIN=python3
 fi
@@ -20,10 +25,12 @@ if [ -x "$(command -v pip3)" ]; then
 else
    exit 2
 fi
-$PIP install --upgrade pip
-$PIP install ipfshttpclient web3 psutil
 
-cd ../..
-mkdir certs
+echo -en "Installing Python3 dependencies... "
+$PIP install --upgrade pip  2>&1 > /dev/null
+$PIP install ipfshttpclient web3 psutil --upgrade 2>&1 > /dev/null
+echo "done"
+
+[ -d certs ] || mkdir certs
 
 touch .init-done
